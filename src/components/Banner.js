5c9462ac3201c684f71f0457af060ceb12672fef
Bhuneshwar Kumar <bhuneshwar197@gmail.com>
2022-06-25 16:13:01 +0530

    added banner file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,9 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const Banner = ({banner}) => {
     return (
-     
         <Carousel fade  >
-
         {banner.end.map((item,index)=>(
             
             <Carousel.Item key={item.image}  id="banner" interval={1000} keyboard={true}>
